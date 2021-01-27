7f75f053815cd27c0d9fb4a8d1b61d2dde262285
lescuer97 <leoescuer@protonmail.com>
2021-01-27 10:17:43 +0100

    simplified function menuTogler and added function
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

--- static/index.js
+++ static/index.js
@@ -1,27 +1,45 @@
-const navSlide = () => {
+
 const burger =  document.querySelector(".burger");
 const nav = document.querySelector(".nav");
-
 const close = document.querySelector(".close")
-const navLinks = document.querySelectorAll('.nav li');
 
-burger.addEventListener('click', () => {
-  //toggle nav
+
+// toggler function of the menu
+const menuToggler = () => {
   nav.classList.toggle('nav-active');
   burger.classList.toggle("disapear");
   close.classList.toggle("appear");
+}
 
-         
-  // navLinks.forEach((link, index) => {
-  //   console.log(index);
-  // })
+const navSlide = () => {
+
+
+document.addEventListener("click", (e) => {
+  const isClickInside = nav.contains(e.target);
+  // check if click is outside nav and if it  didn't came from the burger
+  if(!isClickInside && nav.classList.contains("nav-active") && !e.target.classList.contains("burger")) {
+    
+    menuToggler();
+    };
+// toggles the menu when clicking link and checks for screen size
+  if (e.target.classList.contains("link-button") && window.screen.width < 768) {
+    menuToggler();
+  };
+  });  
+
+burger.addEventListener('click', () => {
+  //toggle nav
+ menuToggler();
+
+ 
 })
 
 close.addEventListener('click', () => {
-  
-    nav.classList.toggle('nav-active');
-  burger.classList.toggle("disapear");
-  close.classList.toggle("appear");
+   menuToggler();
 })
+
+
+
 }
+
 navSlide();
