df2d41c32e9c1c7f86c4ad4c3242845715f21e98
lescuer97 <leoescuer@protonmail.com>
2021-02-26 19:04:05 +0100

    fixed of menu
    Signed-off-by: lescuer97 <leoescuer@protonmail.com>

--- static/index.js
+++ static/index.js
@@ -1,55 +1,60 @@
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", () => {
+//     navigator.serviceWorker
+//       .register("../sw_cached_pages.js")
+//       .then((reg) => {})
+//       .catch((err) => {
+//         console.log(`Service Worker: ${err}`);
+//       });
+//   });
+// }
 
-if('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('../sw_cached_pages.js').then(reg => {
-  
-    }).catch(err => {
-      console.log(`Service Worker: ${err}`)
-    })
-  })
-}
-
-const burger =  document.querySelector(".burger");
+const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav");
-const close = document.querySelector(".close")
-
+const closeSvg = document.querySelector(".close");
 
 // toggler function of the menu
 const menuToggler = () => {
-  nav.classList.toggle('nav-active');
+  nav.classList.toggle("nav-active");
   burger.classList.toggle("disapear");
-  close.classList.toggle("appear");
-}
+  closeSvg.classList.toggle("appear");
+};
 
+const navCloser = () => {
+  document.addEventListener("click", (e) => {
+    e.preventDefault();
+    const isClickInside = nav.contains(e.target);
+    console.log(isClickInside);
+    // check if click is outside nav and if it  didn't came from the burger
+    if (
+      !isClickInside &&
+      nav.classList.contains("nav-active") &&
+      !e.target.classList.contains("burger")
+    ) {
+      console.log("click 1");
+      menuToggler();
+    }
+    // // toggles the menu when clicking link and checks for screen size
+    // if (
+    //   e.target.classList.contains("link-button") &&
+    //   window.screen.width < 768
+    // ) {
+    //   console.log("click 2");
+    //   menuToggler();
+    // }
+  });
+};
 const navSlide = () => {
-
-
-document.addEventListener("click", (e) => {
-  const isClickInside = nav.contains(e.target);
-  // check if click is outside nav and if it  didn't came from the burger
-  if(!isClickInside && nav.classList.contains("nav-active") && !e.target.classList.contains("burger")) {
-    
+  burger.addEventListener("click", (e) => {
+    e.preventDefault();
+    //toggle nav
     menuToggler();
-    };
-// toggles the menu when clicking link and checks for screen size
-  if (e.target.classList.contains("link-button") && window.screen.width < 768) {
+  });
+  closeSvg.addEventListener("click", (e) => {
+    e.preventDefault();
     menuToggler();
-  };
-  });  
-
-burger.addEventListener('click', () => {
-  //toggle nav
- menuToggler();
-
- 
-})
-
-close.addEventListener('click', () => {
-   menuToggler();
-})
-
-
-
-}
+  });
+};
 
 navSlide();
+navCloser();
